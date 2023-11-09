04445e4fb25da08bcc47930059680cf9725ecca0
Scintilla7 <steven.chris03@gmail.com>
2023-11-09 19:32:12 +0700

    first commit
    Signed-off-by: dimas27 <steven.chris03@gmail.com>

---
+++ login_page/lib/main.dart
@@ -0,0 +1,188 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: LoginPage(),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(45),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 145,
+              height: 160,
+              child: Image.asset(
+                'asset/logo_pwl.png',
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Selamat Datang',
+              style: TextStyle(
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w800,
+                fontSize: 17,
+                color: Color(0xFF242F9B),
+              ),
+            ),
+            const SizedBox(height: 108),
+            TextFormField(
+              controller: _emailController,
+              maxLines: 1,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "Email",
+                hintText: "contoh@email.com",
+                prefixIcon: Icon(
+                  Icons.mail,
+                ), // Change the color of the prefix icon
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(), // Change the color of the border
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: _passwordController,
+              maxLines: 1,
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: !_isPasswordVisible,
+              decoration: InputDecoration(
+                labelText: "Password",
+                hintText: "Masukkan Password",
+                prefixIcon: Icon(
+                  Icons.lock,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(),
+                ),
+              ),
+            ),
+            const SizedBox(height: 35),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => submit(
+                  context,
+                  _emailController.text,
+                  _passwordController.text,
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Color(0xFF242F9B),
+                  ), // Replace Colors.red with your desired color
+                ),
+                child: const Text("MASUK"),
+              ),
+            ),
+            SizedBox(height: 200),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Belum punya akun? ',
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontSize: 12,
+                      color: Color(0xFF242F9B),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print('Text is tapped!');
+                      // Navigate to the desired page here
+                    },
+                    child: Text(
+                      'Daftar sekarang',
+                      style: TextStyle(
+                        fontFamily: 'Inter',
+                        fontSize: 12,
+                        color: Color(0xFF242F9B),
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void submit(BuildContext context, String email, String password) {
+    if (email.isEmpty || password.isEmpty) {
+      final snackBar = SnackBar(
+        duration: const Duration(seconds: 5),
+        content: const Text("Email dan Password tidak boleh kosong!"),
+        backgroundColor: Colors.red,
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      return;
+    }
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Login Berhasil"),
+          content: const Text("Selamat! Anda berhasil Login."),
+          actions: [
+            TextButton(
+              child: const Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
+    );
+    return;
+  }
+}
